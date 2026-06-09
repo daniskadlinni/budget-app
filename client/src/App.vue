f1a4c976fa29c417cdd050db2fe0d969a07a0c12
Denis Chernyshev <daniska@yandex.ru>
2026-06-09 13:12:18 +0300

    fix: add loading state while syncing

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <router-view />
+  <div v-if="loading" class="flex flex-center q-page-container" style="height: 100vh">
+    <q-spinner color="primary" size="3em" />
+  </div>
+  <router-view v-else />
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { initStorage } from 'src/utils/storage';
 import { syncFromServer } from 'src/utils/sync';
+
+const loading = ref(true);
 
 onMounted(async () => {
   initStorage();
   await syncFromServer();
+  loading.value = false;
 });
 </script>
